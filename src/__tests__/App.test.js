b45dcd64e2af0fec73afb5c5a29dea63602401e0
tscholl44 <tscholl4444@gmail.com>
2025-05-13 13:50:30 -0400

    Create App.test.js

---
+++ src/__tests__/App.test.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import App from '../App';
+
+
+describe('<App /> component', () => {
+  let AppDOM;
+  beforeEach(() => {
+    AppDOM = render(<App />).container.firstChild;
+  })
+
+
+  test('renders list of events', () => {
+    expect(AppDOM.querySelector('#event-list')).toBeInTheDocument();
+  });
+
+
+  test('render CitySearch', () => {
+    expect(AppDOM.querySelector('#city-search')).toBeInTheDocument();
+  });
+
+  test('render NumberOfEvents', () => {
+    expect(AppDOM.querySelector('#number-of-events')).toBeInTheDocument();
+  });
+  
+});
