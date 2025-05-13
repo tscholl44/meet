a040112d0ee47d8daea116f091908d3ecc8cde7f
tscholl44 <tscholl4444@gmail.com>
2025-05-13 13:50:50 -0400

    Create Event.jsx

---
+++ src/components/Event.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+import { useState } from "react";
+
+const Event = ({ event }) => {
+  const [showDetails, setShowDetails] = useState(false);
+  return (
+    <li className="event">
+      <h2>{event && event.summary}</h2>
+      <p>{event && event.location}</p>
+      <p>{event && (new Date(event.created)).toUTCString()}</p>
+      {showDetails ?
+        <p className="details" data-testid="event-details">{event && event.description}</p> :
+        null
+      }
+      <button className="details-btn" onClick={() => {
+        showDetails ? setShowDetails(false) : setShowDetails(true)
+      }}>{showDetails ? "hide details" : "show details"}</button>
+    </li>
+  )
+}
+
+export default Event;
