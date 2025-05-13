751303d63269daa5bc3231e3ebe3b15cde655f3f
tscholl44 <tscholl4444@gmail.com>
2025-05-13 13:50:35 -0400

    Create EventList.test.js

---
+++ src/__tests__/EventList.test.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+import { getEvents } from '../api';
+import EventList from '../components/EventList';
+
+describe('<EventList /> component', () => {
+ let EventListComponent;
+ beforeEach(() => {
+   EventListComponent = render(<EventList />);
+ })
+
+ test('has an element with "list" role', () => {
+   expect(EventListComponent.queryByRole("list")).toBeInTheDocument();
+ });
+
+ test('renders correct number of events', async () => {
+   const allEvents = await getEvents(); 
+   EventListComponent.rerender(<EventList events={allEvents} />);
+   expect(EventListComponent.getAllByRole("listitem")).toHaveLength(allEvents.length);
+ });
+
+});
