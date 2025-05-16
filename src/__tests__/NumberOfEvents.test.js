cfe2b430ca29652c39e7ebc2521dad29cabbbfc2
tscholl44 <tscholl4444@gmail.com>
2025-05-16 16:46:48 -0400

    integration test for NOE

--- src/__tests__/NumberOfEvents.test.js
+++ src/__tests__/NumberOfEvents.test.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, within, waitFor  } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
 import NumberOfEvents from '../components/NumberOfEvents';
 
@@ -21,7 +21,26 @@ describe('<NumberOfEvents /> component', () => {
     const { getByRole } = render(<NumberOfEvents />);
     const inputElement = getByRole('textbox');
 
-    await user.type(inputElement, '10');
+    await user.type(inputElement, '{backspace}{backspace}10');
     expect(inputElement).toHaveValue('10');
   });
+});
+
+describe('<NumberOfEvents /> integration', () => {
+  test('integration: number of events rendered matches user input', async () => {
+    const { container, getByRole } = render(<App />);
+    const appRootNode = container.firstChild;
+    const numberInputBox = within(appRootNode.querySelector('#number-of-events')).getByRole('spinbutton');
+    // Change the value to 5
+    await userEvent.clear(numberInputBox);
+    await userEvent.type(numberInputBox, '5');
+    // Wait for the event list to update
+    const eventListNode = appRootNode.querySelector('#event-list');
+    await waitFor(() => {
+      const renderedEventItems = within(eventListNode).queryAllByRole('listitem');
+      expect(renderedEventItems.length).toBe(5);
+    });
+  });  
+
+
 });
