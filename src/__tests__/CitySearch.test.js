70a60a160e91e32f3dea50b16d3b433cccec012a
tscholl44 <tscholl4444@gmail.com>
2025-05-16 15:46:43 -0400

    add integration tests

--- src/__tests__/CitySearch.test.js
+++ src/__tests__/CitySearch.test.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import { render, within } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
-import { extractLocations, getEvents } from '../api';
 import CitySearch from '../components/CitySearch';
+import App from '../App';
+import { extractLocations, getEvents } from '../api';
 
 
 describe('<CitySearch /> component', () => {
@@ -65,7 +66,10 @@ describe('<CitySearch /> component', () => {
     const user = userEvent.setup();
     const allEvents = await getEvents(); 
     const allLocations = extractLocations(allEvents);
-    CitySearchComponent.rerender(<CitySearch allLocations={allLocations} />);
+    CitySearchComponent.rerender(<CitySearch 
+      allLocations={allLocations}
+      setCurrentCity={() => { }}
+    />);
 
 
     const cityTextBox = CitySearchComponent.queryByRole('textbox');
@@ -80,5 +84,28 @@ describe('<CitySearch /> component', () => {
 
 
     expect(cityTextBox).toHaveValue(BerlinGermanySuggestion.textContent);
+    
   });
+});
+
+describe('<CitySearch /> integration', () => {
+  test('renders suggestions list when the app is rendered.', async () => {
+    const user = userEvent.setup();
+    const AppComponent = render(<App />);
+    const AppDOM = AppComponent.container.firstChild;
+
+
+    const CitySearchDOM = AppDOM.querySelector('#city-search');
+    const cityTextBox = within(CitySearchDOM).queryByRole('textbox');
+    await user.click(cityTextBox);
+
+
+    const allEvents = await getEvents();
+    const allLocations = extractLocations(allEvents);
+
+
+    const suggestionListItems = within(CitySearchDOM).queryAllByRole('listitem');
+    expect(suggestionListItems.length).toBe(allLocations.length + 1);
+  });
+
 });
