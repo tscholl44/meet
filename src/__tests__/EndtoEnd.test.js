53a8f8c230b6eea64aadc8a4013f830441d9e23f
tscholl44 <tscholl4444@gmail.com>
2025-05-21 16:43:01 -0400

    Create EndtoEnd.test.js

---
+++ src/__tests__/EndtoEnd.test.js
@@ -0,0 +1,37 @@
+import puppeteer from 'puppeteer';
+
+
+describe('show/hide an event details', () => {
+    let browser;
+    let page;
+    beforeAll(async () => {
+        browser = await puppeteer.launch();
+        page = await browser.newPage();
+        await page.goto('http://localhost:5173/'); // If your Vercel app is running in a different port please update it here
+        await page.waitForSelector('.event');
+    });
+
+    afterAll(() => {
+        browser.close();
+    });
+
+
+    test('An event element is collapsed by default', async () => {
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();   
+    });
+    
+    test('User can expand an event to see its details', async () => {
+
+        await page.click('.event .details-btn');
+
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeDefined();
+    });
+
+    test('User can collapse an event to hide details', async () => {
+        await page.click('.event .details-btn');
+        const eventDetails = await page.$('.event .details');
+        expect(eventDetails).toBeNull();
+    });
+});
