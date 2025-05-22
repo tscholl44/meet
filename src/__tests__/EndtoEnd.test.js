2dcef5e8043304938d7c49b32c3db1bd03a74d57
tscholl44 <tscholl4444@gmail.com>
2025-05-22 17:39:30 -0400

    bonus task - feature 1 end to end

--- src/__tests__/EndtoEnd.test.js
+++ src/__tests__/EndtoEnd.test.js
@@ -35,3 +35,38 @@ describe('show/hide an event details', () => {
         expect(eventDetails).toBeNull();
     });
 });
+
+describe('Filter events by city.', () => {
+    let browser;
+    let page;
+    let eventListItems;
+    beforeAll(async () => {
+        browser = await puppeteer.launch({});
+        page = await browser.newPage();
+        await page.goto('http://localhost:5173/');
+        await page.waitForSelector('.event');
+        eventListItems = await page.$$('.event');
+    });
+
+    afterAll(async () => {
+        await browser.close();
+    });
+
+    test('When user hasn’t searched for a city, show upcoming events from all cities.', async () => {
+        expect(eventListItems.length).toBe(32);
+    });
+
+    test('User should see a list of suggestions when they search for a city', async () => {
+        await page.type('.city', 'Berlin');
+        const suggestions = await page.$$('.suggestions li');
+        expect(suggestions.length).toBeGreaterThan(1);
+    });
+
+    test('User can select a city from the suggested list', async () => {
+        await page.type('.city', 'Berlin');
+        const suggestions = await page.$$('.suggestions li');
+        await suggestions[0].click();
+        const selectedCity = await page.$eval('.city', el => el.value);
+        expect(selectedCity).toBe('Berlin');
+    });
+});
