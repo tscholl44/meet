dec6eebabc8c06a59915b6c6600890ff75c98a3b
tscholl44 <tscholl4444@gmail.com>
2025-05-22 17:39:42 -0400

    import atatus

--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import * as atatus from 'atatus-spa';
+
+atatus.config('1a8e393e93fa4074b975ec1fbe728c7c').install();
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
