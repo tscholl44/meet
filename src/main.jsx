a838d1960a1b6f728542863310277a20b51487c9
tscholl44 <tscholl4444@gmail.com>
2025-05-28 16:30:46 -0400

    Update main.jsx
    serviceWorkerRegistration import

--- src/main.jsx
+++ src/main.jsx
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
 import * as atatus from 'atatus-spa';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 atatus.config('1a8e393e93fa4074b975ec1fbe728c7c').install();
 
@@ -10,4 +11,5 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+);
+serviceWorkerRegistration.register();
