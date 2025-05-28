a58567e08ed44972d82562e3227dd7981d7c64a2
tscholl44 <tscholl4444@gmail.com>
2025-05-28 17:07:25 -0400

    Update api.js

--- src/api.js
+++ src/api.js
@@ -68,6 +68,12 @@ export const getEvents = async () => {
     return mockData;
   }
 
+  if (!navigator.onLine) {
+    const events = localStorage.getItem("lastEvents");
+    NProgress.done();
+    return events ? JSON.parse(events) : [];
+  }  
+
   const token = await getAccessToken();
 
 
@@ -77,6 +83,8 @@ export const getEvents = async () => {
     const response = await fetch(url);
     const result = await response.json();
     if (result) {
+      NProgress.done();
+      localStorage.setItem("lastEvents", JSON.stringify(result.events));
       return result.events;
     } else return null;
   }
