da917155216dfdffff0a6a7ea3c27238ecfef316
tscholl44 <tscholl4444@gmail.com>
2025-05-30 14:43:53 -0400

    Update Event.jsx

--- src/components/Event.jsx
+++ src/components/Event.jsx
@@ -7,7 +7,7 @@ const Event = ({ event }) => {
     <li className="event">
       <h2>{event && event.summary}</h2>
       <p>{event && event.location}</p>
-      <p>{event && (new Date(event.created)).toUTCString()}</p>
+      <p >{event && (new Date(event.created)).toUTCString()}</p>
       {showDetails ?
         <p className="details" data-testid="event-details">{event && event.description}</p> :
         null
